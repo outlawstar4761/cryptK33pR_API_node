d3cc0b5fe1da5c29375638bd91d3ac4ee264351d
outlawstar4761 <outlawstar4761@gmail.com>
2018-08-02 10:16:25 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const autobahn = require('autobahn');
+const cryptServer = require('./crypt_server_module');
+const Worker = require('./crypt_server_module/worker');
 const app = express();
 const PORT = 8854;
+const WAMPHOST = 'cryptk33prAPI';
 
-const cryptServer = require('./crypt_server_module');
+var wampConnection = new autobahn.Connection({url:"ws://localhost:8686/ws",realm:'realm1'});
+wampConnection.onopen = function(session){
+    console.log('Connected to WAMP router...');
+    SubscribetoWorkers(session)
+    // session.subscribe('scryptminer2.summary',onMinerData);
+    // session.subscribe('scryptminer2.device',onMinerData);
+    // session.subscribe('scryptminer2.pool',onMinerData);
+    // session.subscribe('scryptminer1.summary',onMinerData);
+    // session.subscribe('scryptminer1.device',onMinerData);
+    // session.subscribe('scryptminer1.pool',onMinerData);
+    session.register(WAMPHOST + '.translateSummary',cryptServer.translateMinerSummary).then((req)=>{console.log(WAMPHOST + '.translateSummary registered.');},console.error);
+    session.register(WAMPHOST + '.translateDevices',cryptServer.translateMinerDevices).then((req)=>{console.log(WAMPHOST + '.translateDevices registered.');},console.error);
+    session.register(WAMPHOST + '.translatePools',cryptServer.translateMinerPools).then((req)=>{console.log(WAMPHOST + '.translatePools registered.');},console.error);
+    // session.call('scryptminer2.getSummary');
+    // session.call('scryptminer1.getSummary');
+}
+
+function SubscribetoWorkers(session){
+    var subscriptions = ['summary','device','pool'];
+    var methods = ['cryptServer.onSummary','cryptServer.onDevice','cryptServer.onPool'];
+    Worker.getAll().then((workers)=>{
+        for(var i = 0; i < workers.length; i++){
+            for(var j = 0; j < subscriptions.length; j++){
+                session.subscribe(workers[i] + '.' + subscriptions[j],methods[j]);
+                console.log('Subscribed to ' + workers[i] + '.' + subscriptions[j]);
+            }
+        }
+    },console.error);
+}
 
 /*Config*/
 app.use(bodyParser.urlencoded({extended:false}));
@@ -70,5 +102,6 @@ app.post('/market_history',cryptServer.createMarketSnapShot);
 
 
 app.listen(PORT,()=>{
-    console.log('Listening on port: ' + PORT);
+    console.log('Rest API Listening on port: ' + PORT);
+    wampConnection.open();
 });
