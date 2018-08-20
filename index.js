f48e1d892a2ed24960e4400ca3ad727343edb219
outlawstar4761 <outlawstar4761@gmail.com>
2018-08-20 10:19:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -40,6 +40,11 @@ function SubscribetoWorkers(session){
 /*Config*/
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, auth_token, request_token, password");
+  next();
+});
 
 
 /*Authentication*/
